15947d957db2efe009e4dd384ae9354e9e0bfea5
kingdrofd <kingdrofd@gmail.com>
2024-01-09 13:05:18 +0100

    Add: -logic to read the json file -logic to display data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:gamesave/utils/directory_handler.dart';
+import 'package:gap/gap.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,79 +14,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _directoryPathController = TextEditingController();
-  List<String> lastSegments = [];
+class _MainPageState extends State<MainPage> {
+  Directories directories = Directories();
+  List<Map<String, dynamic>> jsonData = [];
 
-  void _checkDirectory() {
-    String basePath = _directoryPathController.text;
+  @override
+  void initState() {
+    loadJsonData();
+    super.initState();
+  }
 
-    Directory baseDirectory = Directory(basePath);
-    if (baseDirectory.existsSync()) {
-      List<Directory> subdirectories = baseDirectory
-          .listSync()
-          .where((entity) => entity is Directory)
-          .cast<Directory>()
-          .toList();
+  Future<List<Map<String, dynamic>>> readJsonFile() async {
+    try {
+      String documentDir = await directories.getDocumentsPaths();
+      String filePath = '$documentDir/steam_app_info.json';
+      File file = File(filePath);
+      String fileContent = await file.readAsString();
 
-      lastSegments = subdirectories.map((subdirectory) {
-        List<String> pathSegments =
-            subdirectory.path.split(Platform.pathSeparator);
-        return pathSegments.isNotEmpty ? pathSegments.last : '';
-      }).toList();
-    } else {
-      lastSegments.clear();
+      final jsonData = jsonDecode(fileContent);
+      final data = jsonData['applist']['apps'];
+
+      return List<Map<String, dynamic>>.from(data);
+    } catch (e) {
+      print(e);
+      return [];
     }
+  }
 
-    setState(() {});
+  Future<void> loadJsonData() async {
+    List<Map<String, dynamic>> data = await readJsonFile();
+
+    setState(() {
+      jsonData = data;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Directory Checker'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _directoryPathController,
-              decoration: InputDecoration(labelText: 'Enter directory path'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _checkDirectory,
-              child: Text('Check Directory'),
-            ),
-            SizedBox(height: 20),
-            Text('Last segments of subdirectories:'),
-            Expanded(
-              child: ListView.builder(
-                itemCount: lastSegments.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: SelectableText(lastSegments[index]),
-                    onTap: () {
-                      setState(() {});
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4, // You can adjust the cross-axis count as needed
         ),
+        itemBuilder: (context, index) {
+          if (index < jsonData.length) {
+            return GameInfoCard(
+              jsonData: jsonData,
+              index: index,
+            );
+          } else {
+            return SizedBox.shrink();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class GameInfoCard extends StatelessWidget {
+  const GameInfoCard({
+    super.key,
+    required this.jsonData,
+    required this.index,
+  });
+
+  final List<Map<String, dynamic>> jsonData;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Image.network(
+            '${jsonData[index]['image_link']}',
+            errorBuilder: (context, error, stackTrace) {
+              return Text("No Image");
+            },
+          ),
+          Gap(10),
+          Text('${jsonData[index]["name"]}'),
+          Text('Id: ${jsonData[index]["appid"]}'),
+          Text('Location: ${jsonData[index]["directory"]}'),
+        ],
       ),
     );
   }
