ee611fbeef091cb74d1577cbd2ce159e8b40ffa5
kingdrofd <kingdrofd@gmail.com>
2024-01-11 14:10:54 +0100

    Add & Fix: - Simple ui change - Fixed infinite items in grid view (always read your code before getting angry at your code, computers dont make mistakes :) )

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
   }
@@ -28,11 +29,11 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   Directories directories = Directories();
-  List<Map<String, dynamic>> jsonData = [];
+  late Future<List<Map<String, dynamic>>> jsonDataFuture;
 
   @override
   void initState() {
-    loadJsonData();
+    jsonDataFuture = readJsonFile();
     super.initState();
   }
 
@@ -53,29 +54,60 @@ class _MainPageState extends State<MainPage> {
     }
   }
 
-  Future<void> loadJsonData() async {
-    List<Map<String, dynamic>> data = await readJsonFile();
-
-    setState(() {
-      jsonData = data;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          childAspectRatio: 200 / 100,
-          crossAxisCount: 4,
-          crossAxisSpacing: 8.0,
-          mainAxisSpacing: 8.0,
-        ),
-        itemBuilder: (context, index) {
-          if (index < jsonData.length) {
-            return GameInfoCard(jsonData: jsonData, index: index);
+      backgroundColor: const Color.fromARGB(255, 36, 36, 36),
+      appBar: AppBar(
+        elevation: 10,
+        backgroundColor: const Color.fromARGB(255, 36, 36, 36),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.file_upload,
+              color: Colors.grey,
+              size: 40,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.file_download,
+              color: Colors.grey,
+              size: 40,
+            ),
+          ),
+        ],
+      ),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: jsonDataFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error loading data'));
           } else {
-            return SizedBox.shrink();
+            List<Map<String, dynamic>> jsonData = snapshot.data ?? [];
+            return Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              child: GridView.builder(
+                itemCount: jsonData.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 200 / 100,
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                ),
+                itemBuilder: (context, index) {
+                  if (index < jsonData.length) {
+                    return GameInfoCard(jsonData: jsonData, index: index);
+                  } else {
+                    return SizedBox.shrink();
+                  }
+                },
+              ),
+            );
           }
         },
       ),
@@ -94,21 +126,29 @@ class GameInfoCard extends StatelessWidget {
   final int index;
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      '${jsonData[index]['image_link']}',
-      fit: BoxFit.cover,
-      errorBuilder: (context, error, stackTrace) {
-        return Stack(
-          alignment: Alignment.center,
-          children: [
-            Placeholder(),
-            Text(
-              "No Image",
-              style: TextStyle(fontSize: 30),
-            ),
-          ],
-        );
-      },
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+          '${jsonData[index]['image_link']}',
+          fit: BoxFit.cover,
+          errorBuilder: (context, error, stackTrace) {
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                Placeholder(),
+                Text(
+                  "No Image",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
